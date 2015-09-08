5109e4b4a13f08acf06df6642535f6575b07f0ed
Olivier Gosse-Gardet <ext-ogossegardet@leaseplan.fr>
2015-09-08 21:42:45 +0200

    Added: Default language

--- app/models/storytime/language.rb
+++ app/models/storytime/language.rb
@@ -2,6 +2,10 @@ module Storytime
   class Language < ActiveRecord::Base
   	has_many :posts
   
+  	def self.default
+  		@_default_lang = Storytime::Language.find_by_lang("fr")
+  	end
+
   	def self.regexp
   		@_regexp ||= /#{Storytime::Language.all.map(&:lang).join("|")}/
   	end
