8d6aba9c89fb979fcc9aa7d39882312c916cd58c
LeoPirolo <francesco.pirolo@air-indemnite.com>
2017-06-02 17:29:14 +0200

    Added: Get others posts’ links

--- app/models/storytime/blog_post.rb
+++ app/models/storytime/blog_post.rb
@@ -1,4 +1,19 @@
 module Storytime
   class BlogPost < Post
+    def get_younger_blog_post
+      BlogPost.where('published_at > ?', self.published_at)&.order(:published_at)&.first
+    end
+
+    def get_older_blog_post
+      BlogPost.where('published_at < ?', self.published_at)&.order(:published_at)&.last
+    end
+
+    def get_younger_blog_post_by_2
+      BlogPost.where('published_at > ?', self.published_at)&.order(:published_at)&.first(2)&.last
+    end
+
+    def get_older_blog_post_by_2
+      BlogPost.where('published_at < ?', self.published_at)&.order(:published_at)&.last(2)&.first
+    end
   end
 end
