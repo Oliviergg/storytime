8b04d71244c963f4aba414dddf42ffed741938a5
LeoPirolo <francesco.pirolo@air-indemnite.com>
2017-09-06 14:52:55 +0200

    302 to 410

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
- Storytime::Engine.routes.draw do
+Storytime::Engine.routes.draw do
   resources :subscriptions, only: [:create]
 
   get 'subscriptions/unsubscribe', to: 'subscriptions#destroy', as: 'unsubscribe_mailing_list'
 
-  namespace :dashboard, :path => Storytime.dashboard_namespace_path do
+  namespace :dashboard, path: Storytime.dashboard_namespace_path do
     get '/', to: 'posts#index'
 
-    resources :sites, only: [:new, :edit, :update, :create]
+    resources :sites, only: %i[new edit update create]
 
     resources :posts do
       resources :autosaves, only: [:create]
     end
 
     resources :snippets, except: [:show]
-    resources :media, except: [:show, :edit, :update]
-    resources :imports, only: [:new, :create]
+    resources :media, except: %i[show edit update]
+    resources :imports, only: %i[new create]
     resources :subscriptions
     resources :users, path: Storytime.user_class_underscore.pluralize
 
@@ -32,14 +32,14 @@
 
   get Storytime.home_page_path, Storytime.home_page_route_options
 
-  scope '/(:locale)', :locale => Storytime::Language.regexp do
+  scope '/(:locale)', locale: Storytime::Language.regexp do
     resources :blog_posts, path: '/blog', only: :index
   end
 
-  scope '/(:locale)', :locale => Storytime::Language.regexp do
-    constraints -> (request) {
+  scope '/(:locale)', locale: Storytime::Language.regexp do
+    constraints ->(request) {
       page_name = request.params[:id]
-      (page_name != Storytime.home_page_path) && Storytime::Page.friendly.exists?(page_name)
+      (page_name != Storytime.home_page_path) && Storytime::Page.friendly.exist?(page_name)
     } do
       get '/:category/:id', to: 'pages#show'
     end
